79243378bf304af2ff4ceb66b92b48ddcfb5a38a
PRAJYOT <prajyotnarulkar@gmail.com>
2025-02-15 16:00:25 +0530

    creating a workspace

---
+++ workspace/workspace.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.50"  # Ensure latest provider version
+    }
+  }
+}
+
+provider "tfe" {
+  token = var.TFC_TOKEN  # Uses API token for authentication
+}
+
+variable "TFC_TOKEN" {
+  description = "Terraform Cloud API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "org_name" {
+  default     = "org-tfc"  # Change this to your Terraform Cloud organization name
+  description = "Terraform Cloud Organization Name"
+}
+
+resource "tfe_workspace" "example" {
+  name         = "ec2-deploy"
+  organization = var.org_name
+#   execution_mode = "remote"  # Options: remote, local, agent
+}
+
+output "workspace_id" {
+  value = tfe_workspace.example.id
+}
